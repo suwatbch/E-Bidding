c92a3aa786778e2513f53ee5a57ad143c05c3388
suwatbch <suwat.bch@gmail.com>
2025-05-30 13:50:38 +0700

    update

--- src/app/(main)/user/page.tsx
+++ src/app/(main)/user/page.tsx
@@ -379,7 +379,7 @@ export default function UserPage() {
                   <tr>
                     <th scope="col" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
                       <div className="flex items-center justify-center gap-2">
-                        <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14" />
                         </svg>
                         ลำดับ
@@ -487,9 +487,7 @@ export default function UserPage() {
                         <td className="px-6 py-4 text-center">
                           {user.is_locked ? (
                             <div className="flex justify-center items-center text-red-400">
-                              <svg className="w-6 h-6 hover:scale-110 transition-transform duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-                              </svg>
+                              <LockTableIcon />
                             </div>
                           ) : (
                             <div className="text-sm text-gray-500">
