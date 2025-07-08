fd6f0ca5ef6774710e9a571f8f2d3fb4e737d34d
suwatbch <suwat.bch@gmail.com>
2025-07-08 07:57:47 +0700

    update login

--- src/api/controllers/auth.js
+++ src/api/controllers/auth.js
@@ -5,6 +5,8 @@ const {
   requestOtp,
   resetPassword,
 } = require('../helper/authHelper');
+const { getAllAuctions } = require('../helper/auctionsHelper');
+const { getDateTimeUTCNow } = require('../globalFunction');
 
 // POST /api/auth/login - เข้าสู่ระบบ (Public Route)
 router.post('/login', async (req, res) => {
@@ -21,11 +23,42 @@ router.post('/login', async (req, res) => {
     const result = await loginUser(username, password);
 
     if (result.success) {
-      res.status(200).json({
-        success: true,
-        message: null,
-        data: result.data,
-      });
+      const userData = result.data.user;
+      const userId = userData.user_id;
+      const userType = userData.type;
+      const dateNow = new Date();
+      const startDate = dateNow.toISOString().split('T')[0] + ' 00:00:00';
+      const endDate = dateNow.toISOString().split('T')[0] + ' 23:59:59';
+
+      if (userType === 'admin') {
+        res.status(200).json({
+          success: true,
+          message: null,
+          data: result.data,
+        });
+      } else {
+        // ดึงข้อมูลประมูลทั้งหมด (รองรับการกรองตามวันที่และ user role)
+        const auctionsData = await getAllAuctions(
+          startDate,
+          endDate,
+          userId,
+          userType
+        );
+
+        if (auctionsData.success && auctionsData.data.length > 0) {
+          res.status(200).json({
+            success: true,
+            message: null,
+            data: result.data,
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message:
+              auctionsData.error || 'ไม่พบตลาดประมูลที่สามารถเข้าร่วมได้',
+          });
+        }
+      }
     } else {
       res.status(200).json({
         success: true,
